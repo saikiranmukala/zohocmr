52c8102a93e8dfb080c8ba3887277abf16245ebc
rakshithab457-hue <rakshithab457@gmail.com>
2026-04-13 08:56:15 +0530

    campaign

--- src/test/java/com/zoho/testscript/Darshan.java
+++ src/test/java/com/zoho/testscript/Darshan.java
@@ -4,11 +4,14 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import com.zoho.generic.BaseClass;
+import com.zoho.pom.CampaignPage;
 
 public class Darshan extends BaseClass{
 
 	@Test
 	public void createcampin() throws InterruptedException {
 		Reporter.log("created", true);
+		CampaignPage cp = new CampaignPage(driver);
+		cp.getNewCampaignBt().click();		
 
 }}
