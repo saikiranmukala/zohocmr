96837950bb40a31b5e3aa3faaa49672707e92e41
nsandeep6 <sandeep89110@gmail.com>
2026-04-13 10:52:05 +0530

    addedee

--- src/test/java/com/zoho/testscript/nidhi.java
+++ src/test/java/com/zoho/testscript/nidhi.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class nidhi {
 	@Test
 	public void createcampin() throws InterruptedException {
-		Reporter.log("created newS", true);
+		Reporter.log("created new", true);
 
 		
 }
