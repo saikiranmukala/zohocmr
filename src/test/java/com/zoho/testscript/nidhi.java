bc819c668dced759ad177134dfbdecfb10884364
Nidhi-011998 <sulladmathnidhi@gmail.com>
2026-04-13 08:57:03 +0530

    edited Nidhi testcase

--- src/test/java/com/zoho/testscript/nidhi.java
+++ src/test/java/com/zoho/testscript/nidhi.java
@@ -3,10 +3,21 @@ package com.zoho.testscript;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class nidhi {
+import com.zoho.generic.BaseClass;
+import com.zoho.pom.CampaignPage;
+import com.zoho.pom.HomePage;
+
+public class nidhi extends BaseClass {
 	@Test
 	public void createcampin() throws InterruptedException {
 		Reporter.log("created", true);
+		HomePage h = new HomePage(driver);
+		h.getCampaign().click();
+		
+		CampaignPage cp = new CampaignPage(driver);
+		cp.getNewCampaignBt().click();
+		
+	
 
 }
 }
